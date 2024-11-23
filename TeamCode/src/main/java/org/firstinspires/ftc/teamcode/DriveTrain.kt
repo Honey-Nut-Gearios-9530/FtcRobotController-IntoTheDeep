d21afa9cafa0aa53c90548c47788f4081bf4eab0
AGOODFROG <cadenjquirion@gmail.com>
2024-11-23 13:47:43 -0500

    debuging git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.kt
@@ -4,11 +4,15 @@ import com.qualcomm.robotcore.eventloop.opmode.*
 import com.qualcomm.robotcore.hardware.Gamepad
 import org.firstinspires.ftc.teamcode.modular.*
 
-@TeleOp(name = "DriveTrain")
+@TeleOp(name = "Drive Train")
 // @Disabled
 class DriveTrain : BaseLinearOpMode() {
-    private var power = ToggleableState(1.0, 0.25)
+    private var power = ToggleableState(1, 0.25)
     private lateinit var gp1: GamepadState
+   // private var powerModes = arrayOf(1.00, 0.66, 0.33)
+    //private var powerModeIndex = 0
+
+
 
     override fun runOpMode() {/* Initialization */
         gp1 = GamepadState(gamepad1)
@@ -19,11 +23,27 @@ class DriveTrain : BaseLinearOpMode() {
 
         this.initDriveTrain()
 
+
         /* End Initialization */
         this.waitForStart()
 
         while (this.opModeIsActive()) {
             this.gp1.cycle()
+/*          //Left Bumper: cycle powerMode down
+            if(gp1.current.left_bumper)
+            {
+                powerModeIndex++
+            }
+            if(gp1.current.right_bumper)
+            {
+                powerModeIndex--
+            }
+            if(powerModeIndex > powerModes.size -1)// prevents out of bounds errors
+            {
+                powerModeIndex = 0
+            }
+
+ */
 
             val motorPower = arrayOf(
                 this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + this.gp1.current.right_stick_x,
@@ -31,6 +51,8 @@ class DriveTrain : BaseLinearOpMode() {
                 -this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + this.gp1.current.right_stick_x,
                 this.gp1.current.left_stick_x - this.gp1.current.left_stick_y - this.gp1.current.right_stick_x,
             )
+
+
 
             toggleButtonMap.forEach { it.key.ifIsToggled(it.value) }
 
