cbec1ba06327c5179c3345f0125dcd71a24c8199
AGOODFROG <cadenjquirion@gmail.com>
2024-12-03 19:32:33 -0500

    refactoring code not compleat broke somthing???

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveTrain .kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveTrain .kt
@@ -1,5 +1,7 @@
+
 package org.firstinspires.ftc.teamcode
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.Gamepad
 import org.firstinspires.ftc.teamcode.modular.BaseLinearOpMode
@@ -8,35 +10,22 @@ import org.firstinspires.ftc.teamcode.modular.GamepadState
 import org.firstinspires.ftc.teamcode.modular.ToggleableState
 import kotlin.math.abs
 
-@TeleOp(name = "autoDriveTrain")
+@TeleOp(name = "DriveTrain")
 
-class DriveTrain : BaseLinearOpMode() {
+class AutoDriveTrain  : BaseLinearOpMode() {
     // kotlin does not do numeric type promotion, if the 3rd arg is just "1" than T cannot be inferred
     private var power = ToggleableState(2, 0.33, 0.67, 1.0)
-
     private lateinit var gp1: GamepadState
-    private var leftTurn = this.gamepad1.left_trigger
-    private var rightTurn = this.gamepad1.right_trigger
-    private var forwardMotion = this.gamepad1.left_stick_y
-    private var crabMotion = this.gamepad1.left_stick_x
-    private var amr = 0 // no idea
-
-    private fun update(){
-        leftTurn = this.gp1.current.left_trigger
-        rightTurn = this.gp1.current.right_trigger
-        forwardMotion = this.gp1.current.left_stick_y
-        crabMotion = this.gp1.current.left_stick_x
-    }
 
 
     override fun runOpMode() {/* Initialization */
         telemetry.msTransmissionInterval = 100
 
-
+        gp1 = GamepadState(gamepad1)
 
         val toggleButtonMap = mapOf(
-            GamepadButton(GamepadState(gamepad1), Gamepad::left_bumper) to power::left,
-            GamepadButton(GamepadState(gamepad1), Gamepad::right_bumper) to power::right
+            GamepadButton(gp1, Gamepad::left_bumper) to power::left,
+            GamepadButton(gp1, Gamepad::right_bumper) to power::right
         )
 
         this.initDriveTrain()
@@ -45,21 +34,21 @@ class DriveTrain : BaseLinearOpMode() {
         this.waitForStart()
 
         while (this.opModeIsActive()) {
+            this.gp1.cycle()
             //this.gp1.cycle()
-            update()
 
             toggleButtonMap.forEach { it.key.ifIsToggled(it.value) }
 
             /* Calculates motor power in accordance with the allMotors array
                and formulas found here: https://github.com/brandon-gong/ftc-mecanum
              */
-            val turnPower = -rightTurn + leftTurn
+            val turnPower = -gp1.current.right_trigger + gp1.current.left_trigger
 
             val motorPower = arrayOf(
-                crabMotion - forwardMotion - turnPower,
-                -crabMotion - forwardMotion + turnPower,
-                -crabMotion - forwardMotion - turnPower,
-                crabMotion - forwardMotion + turnPower,
+                this.gp1.current.left_stick_x - this.gp1.current.left_stick_y - turnPower,
+                -this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + turnPower,
+                -this.gp1.current.left_stick_x - this.gp1.current.left_stick_y - turnPower,
+                this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + turnPower,
             )
 
             // Magnitude of the maximum value, not velocity
