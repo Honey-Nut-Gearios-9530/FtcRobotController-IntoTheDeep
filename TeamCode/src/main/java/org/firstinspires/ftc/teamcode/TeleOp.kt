7fcfe46f8d95e5aa664255c25298dd76e716027f
jmccubbin25 <jmccubbin25@gunston.org>
2025-01-25 16:03:43 -0500

    lower teleop elevator speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.kt
@@ -175,7 +175,7 @@ class TeleOp : BaseLinearOpMode() {
 
             // elevator and spinner
             val elevatorPower = gp2.current.left_stick_y.toDouble()
-            elevator.power = if (elevatorPower > 0) elevatorPower * 0.9 else elevatorPower * 0.5
+            elevator.power = if (elevatorPower > 0) elevatorPower * 0.75 else elevatorPower * 0.5
             if (gp2.current.left_stick_y.sign != 0f) spinner.on(gp2.current.left_stick_y) else spinner.off()
 
             lastSwitch = currSwitch
